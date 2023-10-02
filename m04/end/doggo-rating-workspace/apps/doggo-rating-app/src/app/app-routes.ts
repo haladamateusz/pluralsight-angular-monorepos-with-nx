0626837625d5d0bcf4efc87f43649e1e474a8aa2
Mateusz Halada <mateusz.halada@ch.gudel.com>
2023-10-02 11:30:51 +0200

    Updated nx to v16

--- m04/end/doggo-rating-workspace/apps/doggo-rating-app/src/app/app-routes.ts
+++ m04/end/doggo-rating-workspace/apps/doggo-rating-app/src/app/app-routes.ts
@@ -1,16 +1,10 @@
 import { Routes } from '@angular/router';
 import { CallbackComponent } from '@ps-doggo-rating/shared/util-auth';
-import { BuildableTestModule } from '@ps-doggo-rating/buildable-test';
 export const APP_ROUTES: Routes = [
   {
     path: 'doggos',
     loadChildren: () =>
       import('@ps-doggo-rating/doggos/feature').then((m) => m.DOGGOS_ROUTES),
-  },
-  {
-    path: 'about',
-    loadChildren: () =>
-      import('@ps-doggo-rating/about/feature').then((m) => m.ABOUT_ROUTES),
   },
   {
     path: 'callback',
